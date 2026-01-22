9fe5c0fc2a82fdb831655924435a55c097fe52a1
Ajinkya Mishra <ajinkyamishra5555@gmail.com>
2026-01-22 22:59:10 +0530

    Backend Setup

--- app.js
+++ app.js
@@ -21,6 +21,53 @@ app.get('/', (req, res) => {
   res.render('index' , { title: "Chess Game" });
 })
 
+io.on("connection", (uniquesocket) => {
+  console.log("connected");
+  
+  if(!players.white){
+    players.white = uniquesocket.id;
+    uniquesocket.emit("playerColor", "w");
+  }
+  
+  else if(!players.black){
+    players.black = uniquesocket.id;
+    uniquesocket.emit("playerColor", "b");
+  }
+
+  else
+    uniquesocket.emit("spectator role");
+  
+  uniquesocket.on("disconnect", () => {
+    if(uniquesocket.id === players.white) {
+      delete players.white;
+    }
+    else if(uniquesocket.id === players.black) {
+      delete players.black;
+    }
+  })
+
+  uniquesocket.on("move", (move) => {
+    try {
+      if(chess.turn() === 'w' && uniquesocket.id === players.white ) return;
+      if(chess.turn() === 'b' && uniquesocket.id === players.black ) return;
+
+      const result = chess.move(move);
+      if(result) {
+        currentPlayer = chess.turn()
+        io.emit("move", move);
+        io.emit("boardState", chess.fen())
+      }
+      else{
+        console.log("Invalid Move :", move);
+        uniquesocket.emit("Invalid Move:", move);
+      }
+    } catch (error) {
+      console.log(error)
+      uniquesocket.emit("Invalid Move :", move)
+    }
+  })
+});
+
 server.listen(3000, () => {
   console.log('Server is running on port 3000');
 })
